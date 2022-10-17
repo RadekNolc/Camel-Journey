05686c906460c04e4ebb51ef27a95fd44b9dc8af
rnolc <radek.nolc@icloud.com>
2022-10-17 15:07:36 +0200

    Revert "dokumentance - Location + Main"
    This reverts commit 65637f8ada7e0c26a6ecbaf6c16f7795855f1a3a.

--- app/Location.java
+++ app/Location.java
@@ -3,61 +3,42 @@ import java.util.Collections;
 import java.util.List;
 
 //Definice lokace, přidání ID k dané lokaci, k bodu by nemělo smysl
-
-/** Definition of location, matching an ID with the location*/
 public class Location extends Point {
     
     //definice atributu s ID
-    /**Setting new private attributes*/
     private int id;
     protected static int nextId = 1;
 
-/**Creating a new ArrayList for future locations*/
     private static ArrayList<Location> locations = new ArrayList<>();
 
     //prostě konstruktor, IDčko se přiřazuje v potomcích
-    /**Just a constructor
-     * coordinates X,Y in the Cartesian system
-     * @param x
-     * @param y*/
     public Location(int x, int y) {
         super(x, y);
         locations.add(this);
     }
 
-/**Setting a new ID
- * @para, id*/
     public void setId(int id) {
         this.id = id;
     }
 
-/**Getter...*/
     public int getId() {
         return id;
     }
-/**Simple calculator of a direct distance from a current location to the final destianation
- * @param Location ....?
- * @param destination ...?
- * */
+
     public double calculateDirectDistance(Location destination) {
         return Math.sqrt(Math.abs((getX() - destination.getX()) * (getX() - destination.getX())) + Math.abs((getY() - destination.getY()) * (getY() - destination.getY())));
     }
     
-   /** ? */ 
     public static List<Location> getLocations() {
         return Collections.unmodifiableList(locations);
     }
 
-/**Location finder by using ID
- * ...snad?
- * @param id*/
     public static Location getLocationById(int id) throws Exception {
         Location location = locations.get(id - 1);
-        
         if (location != null) {
             return location;
         }
 
-        throw new Exception("Could not find Location with ID: " + id);
+        throw new Exception("Could not find Location with ID " + id);
     }
 }
--- app/Main.java
+++ app/Main.java
@@ -8,16 +8,6 @@ public class Main {
         sc = new Scanner(file);*/
         
         //Vytvoření lokací
-        /**Creating a new location - oasis and warehauses
-         * Oases = key destinations for our business, camels and aopportunities for camels to recover and reply their energy reserves (if necessary)
-         * Warehauses (Storages)= Starting points of our camels and the place to which the camels return to replenish further supplies
-         * 
-         * @param of Oases: Coordinate X, Coordinate Y
-         * @param of warehauses (Storage): Coordinate X, Coordinate Y, the number of containers tha will be/are being refilled, the time it takes to refill, the time for loading/unloading containers on camel
-         * 
-         * ...snad?
-         *  --- asi bych zmenil storages na warehause at je to i v tom jazyce srovnany
-          */
         Location obj1 = new Storage(10, 10, 3, 1000, 1);
         Location obj2 = new Oasis(0, 0);
         Location obj3 = new Oasis(0, 20); //TODO: Změnit na 0, 10
@@ -27,9 +17,6 @@ public class Main {
         Location obj7 = new Storage(50, 50, 3, 1000, 1);
 
         //vytvoření cest
-        /**Creating a new paths to connect locations with warehauses (storages*)
-         *@param of Path: indices i,j indicating places (Oasis, warehause)
-         */
         Path path12 = new Path(1, 2);
         Path path13 = new Path(1, 3);
         Path path14 = new Path(1, 4);
@@ -38,22 +25,9 @@ public class Main {
         Path path67 = new Path(6, 7);
 
         //Vytvoření šablon pro velbloudy
-        /**Creating  templates of camels that represent individual camels with different features
-         *@param of a each cameltemplate: type of camel, min speed, max speed, min possible distance of the species, max possible distance of the species, time the species need to recover, max number of containers that a certain type of camel can load, proportion of species in the herd (interval from 0 to 1, including)
-          
-          * mozna by bylo lepsi stylem a,b,c...?
-          * a=...
-          * b...
-          * c...
-          
-          */
-        
         CamelTemplate pomaly = new CamelTemplate("Pomaly", 5, 10, 10, 50, 0, 1, 0.7);
         CamelTemplate ziznivy = new CamelTemplate("Ziznivy", 10, 10, 1, 1, 10, 1, 0.30);
-        
-/**Receiving requests
- * @param of a request: time of arrival of the request, oasis index as the final destination of the request, number of containers required, expiry time)
- */
+
         Request request1 = new Request(0, 2, 1, 1000);
 
         //Run
