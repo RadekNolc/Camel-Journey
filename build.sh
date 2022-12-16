4fd69cc2bf4bc6b7bd76164e3303fabce3cc1ac0
Radek Nolč <radek.nolc@icloud.com>
2022-12-16 20:13:49 +0100

    Moved to IntelliJ

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd app/source/cz/radeknolc/cameljourney
-javac -d ./build *
-cd build
-jar cmvf ../../../../META-INF/MANIFEST.MF CamelJourney.jar *
-mv CamelJourney.jar ../../../../../../CamelJourney.jar
-cd ..
-rm -r build
-echo "Done."
